9507cca5abe9d953428cf85f67f2fb1cd85cebc7
Alisson G Costa <alisson.costa84200@gmail.com>
2026-04-24 20:20:10 -0300

    finalmente criando o banco de dados

---
+++ src/main/java/br/com/costa/spring_boot_essentials/handler/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package br.com.costa.spring_boot_essentials.handler;
+
+
+import br.com.costa.spring_boot_essentials.exception.BadRequestException;
+import br.com.costa.spring_boot_essentials.exception.ErrorResponse;
+import br.com.costa.spring_boot_essentials.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> BadRequestException(BadRequestException exception) {
+        ErrorResponse response =  ErrorResponse.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+
+    }
+
+
+
+
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException exception) {
+        ErrorResponse response =  ErrorResponse.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.NOT_FOUND.value())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        ErrorResponse response =  ErrorResponse.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+
+    }
+}
